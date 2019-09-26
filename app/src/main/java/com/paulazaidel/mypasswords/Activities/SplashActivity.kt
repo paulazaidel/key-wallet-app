3e4f6c4425789bb56489220494ca8cba5143822f
Paula Zaidel <paulazaaidel@gmail.com>
2019-09-26 19:07:47 -0300

    #5 SplashActivity

---
+++ app/src/main/java/com/paulazaidel/mypasswords/Activities/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.paulazaidel.mypasswords.Activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
